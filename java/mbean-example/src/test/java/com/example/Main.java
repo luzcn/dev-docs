4fcad153f0067000d6142c44355dbc6e04e1394f
Zheng Lu <zheng.lu@oracle.com>
2017-10-23 15:24:25 -0700

    added java mbean example

---
+++ java/mbean-example/src/test/java/com/example/Main.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import javax.management.*;
+import java.lang.management.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        ObjectName name = new ObjectName("com.example:type=Hello");
+
+        Hello mbean = new Hello();
+        mbs.registerMBean(mbean, name);
+
+
+        System.out.println("Waiting forever...");
+        Thread.sleep(Long.MAX_VALUE);
+    }
+}
